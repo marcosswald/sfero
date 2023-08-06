05cb084cb089e24db62b058f6d98d9499939b91a
Marc <tjorven.havregryn@gmail.com>
2023-08-06 13:12:00 +0200

    serial command to set oscillator frequency

--- m0_code/serial_servo_controller/serial_servo_controller.ino
+++ m0_code/serial_servo_controller/serial_servo_controller.ino
@@ -21,7 +21,7 @@ void setup() {
 
   for (int n = 0; n < NR_PWM_DRIVERS; n++) {
     pwm[n].begin();
-    pwm[n].setOscillatorFrequency(27000000);
+    pwm[n].setOscillatorFrequency(25000000);
     pwm[n].setPWMFreq(SERVO_FREQ);
 
     delay(10);
@@ -52,20 +52,50 @@ void loop() {
     int opcode = atoi(chptr);
     switch (opcode) {
 
-      // 0x00 - Reset all pwm channels
-      case 0x00:
+      // 0x0FF - Reset all pwm channels
+      case 0xFF:
         {
-          Serial.println("[0x00] Reset");
+          Serial.println("[0xFF] Reset");
           for (int n = 0; n < NR_PWM_DRIVERS; n++) {
             for (int i = 0; i < 8; i++) {
               pwm[n].setPWM(i, 0, 0);
-              delay(100);
+              delay(10);
             }
           }
           break;
         }
 
-      // 0x01 - Set servo pwm and release: {addr, servo, pulse}
+      // 0x00 - Set oscillator frequency: {board, frequency}
+      case 0x00:
+        {
+          int tokens = 2;
+          int comarray[tokens];
+          int board, freq = 0;
+          if (parseTokens(comarray, tokens)) {
+            board = comarray[0];
+            freq = comarray[1];
+            Serial.print("[0x00] board=");
+            Serial.print(board, DEC);
+            Serial.print(", freq=");
+            Serial.print(freq, DEC);
+            Serial.println();
+
+            // set pulse
+            if (board < NR_PWM_DRIVERS) {
+              //pwm[board].begin();
+              pwm[board].reset();
+              pwm[board].begin();
+              pwm[board].setOscillatorFrequency(freq);
+              pwm[board].setPWMFreq(SERVO_FREQ);
+            }
+
+          } else {
+            Serial.println("Error in opcode (0x00)");
+          }
+          break;
+        }
+
+      // 0x01 - Set servo pwm and release: {board, servo, pulse}
       case 0x01:
         {
           int tokens = 3;
@@ -96,7 +126,7 @@ void loop() {
           break;
         }
 
-      // 0x02 - Set servo pwm and keep position: {addr, servo, pulse}
+      // 0x02 - Set servo pwm and keep position: {board, servo, pulse}
       case 0x02:
         {
           int tokens = 3;
@@ -112,6 +142,9 @@ void loop() {
             Serial.print(servo, DEC);
             Serial.print(", pulse=");
             Serial.print(pulse, DEC);
+            Serial.print(", freq=");
+            int freq = pwm[board].getOscillatorFrequency();
+            Serial.print(freq, DEC);
             Serial.println();
 
             // set pulse
